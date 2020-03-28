618ff5fe38bdc3baf0740fbb3ca1f91d4ecfb0d5
ErickVO <59853887+ErickVO@users.noreply.github.com>
2020-03-28 11:37:33 -0400

    Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudioEA.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudioEA.DAL
+{
+   public class Contexto : DbContext
+    {
+        public DbSet<Articulos> Articulos { get; set; }
+        public DbSet<Categorias> Categorias { get; set; }
+        public DbSet<Eventos> Eventos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = DAL\StudioEA.db");
+        }
+    }
+}
