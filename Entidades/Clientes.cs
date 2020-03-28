9c3f9733a58838176afdb331051682adbc82ae73
Anthony Brian Jimenez Lopez <anthony_jimenez2@ucne.edu.do>
2020-03-28 11:28:01 -0400

    Creando entidad cliente

---
+++ Entidades/Clientes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace StudioEA.Entidades
+{
+    public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+        public int UsuarioId { get; set; }
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+        public String Cedula { get; set; }
+        public String Direccion { get; set; }
+        public String Telefono { get; set; }
+        public String Celular { get; set; }
+        public String Sexo { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+
+        [ForeignKey("ClienteId")]
+        public List<VentasDetalle> VentasDetalles { get; set; }
+
+        public Clientes()
+        {
+            ClienteId = 0;
+            UsuarioId = 0;
+            Nombre = string.Empty;
+            Apellido = string.Empty;
+            Cedula = string.Empty;
+            Direccion = string.Empty;
+            Telefono = string.Empty;
+            Celular = string.Empty;
+            Sexo = string.Empty;
+            FechaNacimiento = DateTime.Now;
+            VentasDetalles = new List<VentasDetalle>();
+        }
+    }
+}
