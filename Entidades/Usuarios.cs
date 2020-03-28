cc9ab96a10228454080b6b7a661048d8d2371e25
Anthony Brian Jimenez Lopez <anthony_jimenez2@ucne.edu.do>
2020-03-28 11:18:22 -0400

    Creando entidad usuario

---
+++ Entidades/Usuarios.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace StudioEA.Entidades
+{
+    public class Usuarios
+    {
+        [Key]
+        public int UsuarioId { get; set; }
+        public String NombreUsuario { get; set; }
+        public String Contrasena { get; set; }
+
+        public Usuarios()
+        {
+            UsuarioId = 0;
+            NombreUsuario = string.Empty;
+            Contrasena = string.Empty;
+        }
+    }
+}
