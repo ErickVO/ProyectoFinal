0529d9686007de30adb675c9808f84907e83dd09
Anthony Brian Jimenez Lopez <anthony_jimenez2@ucne.edu.do>
2020-03-28 11:32:43 -0400

    Creando la entidad fotografo

---
+++ Entidades/Fotografos.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace StudioEA.Entidades
+{
+    public class Fotografos
+    {
+        [Key]
+        public int FotografoId { get; set; }
+        public int UsuarioId { get; set; }
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+        public String Cedula { get; set; }
+        public String Direccion { get; set; }
+        public String Telefono { get; set; }
+        public String Celular { get; set; }
+        public String Sexo { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public decimal Sueldo { get; set; }
+
+        [ForeignKey("FotografoId")]
+        public List<VentasDetalle> VentasDetalles { get; set; }
+
+        public Fotografos()
+        {
+            FotografoId = 0;
+            UsuarioId = 0;
+            Nombre = string.Empty;
+            Apellido = string.Empty;
+            Cedula = string.Empty;
+            Direccion = string.Empty;
+            Telefono = string.Empty;
+            Celular = string.Empty;
+            Sexo = string.Empty;
+            FechaNacimiento = DateTime.Now;
+            Sueldo = 0;
+            VentasDetalles = new List<VentasDetalle>();
+        }
+    }
+}
