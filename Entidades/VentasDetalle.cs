7be838818d05cd57990476d522389a5edb678e92
Anthony Brian Jimenez Lopez <anthony_jimenez2@ucne.edu.do>
2020-03-28 11:35:10 -0400

    Creando la venta detalle

---
+++ Entidades/VentasDetalle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace StudioEA.Entidades
+{
+    public class VentasDetalle
+    {
+        [Key]
+        public int VentasDetalleId { get; set; }
+        public int ClienteId { get; set; }
+        public int FotografoId { get; set; }
+        public int ArticuloId { get; set; }
+        public int CantidadArticulo { get; set; }
+        public int EventoId { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal Monto { get; set; }
+        public decimal MontoTotal { get; set; }
+
+        public VentasDetalle()
+        {
+            VentasDetalleId = 0;
+            ClienteId = 0;
+            FotografoId = 0;
+            ArticuloId = 0;
+            CantidadArticulo = 0;
+            EventoId = 0;
+            Fecha = DateTime.Now;
+            Monto = 0;
+            MontoTotal = 0;
+        }
+    }
+}
