31929b5c83fd49d4868b487ffefb7dd4fd571b0e
ErickVO <59853887+ErickVO@users.noreply.github.com>
2020-03-29 16:52:26 -0400

    Configuracion de los Registros

--- UI/Registros/RegistroArticulos.xaml.cs
+++ UI/Registros/RegistroArticulos.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using StudioEA.BLL;
+using StudioEA.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -17,9 +19,107 @@ namespace StudioEA.UI.Registros
     /// </summary>
     public partial class RegistroArticulos : Window
     {
+
+        Articulos articulos = new Articulos();
+
         public RegistroArticulos()
         {
             InitializeComponent();
+            this.DataContext = articulos;
+            ArticuloIdTextBox.Text = "0";
+        }
+
+        private void Cargar()
+        {
+            this.DataContext = null;
+            this.DataContext = articulos;
+        }
+
+        private void Limpiar()
+        {
+            ArticuloIdTextBox.Text = "0";
+            UsuarioIdTextBox.Text = string.Empty;
+            DescripcionTextBox.Text = string.Empty;
+            CategoriasTextBox.Text = string.Empty;
+            StockTextBox.Text = string.Empty;
+            PrecioTextBox.Text = string.Empty;
+            CostoTextBox.Text = string.Empty;
+            articulos.ComprasDetalle = new List<ComprasDetalle>();
+            articulos = new Articulos();
+            Cargar();
+        }
+
+        private void NuevoButton_Click(object sender, RoutedEventArgs e)
+        {
+            Limpiar();
+        }
+
+        private bool ExisteEnLaBaseDeDatos()
+        {
+            Articulos a = ArticulosBLL.Buscar(articulos.ArticuloId);
+
+            return (a != null);
+        }
+
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            bool paso = false;
+
+            if (articulos.ArticuloId == 0)
+            {
+                paso = ArticulosBLL.Guardar(articulos);
+            }
+            else
+            {
+                if (ExisteEnLaBaseDeDatos())
+                {
+                    paso = ArticulosBLL.Modificar(articulos);
+                }
+                else
+                {
+                    MessageBox.Show("No Existe en la base de datos", "ERROR");
+                    return;
+                }
+
+                if (paso)
+                {
+                    Limpiar();
+                    MessageBox.Show("Guardado!!", "Exito");
+                }
+                else
+                {
+                    MessageBox.Show("No se pudo Guardar", "ERROR");
+                }
+            }
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ArticulosBLL.Eliminar(articulos.ArticuloId))
+            {
+                Limpiar();
+                MessageBox.Show("Eliminado", "EXITO");
+            }
+            else
+            {
+                MessageBox.Show("No se pudo eliminar", "Error");
+            }
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            Articulos a = ArticulosBLL.Buscar(articulos.ArticuloId);
+
+            if (a != null)
+            {
+                articulos = a;
+                Cargar();
+            }
+            else
+            {
+                MessageBox.Show("No se encontro", "ERROR");
+            }
         }
     }
 }
--- UI/Registros/RegistroCategorias.xaml.cs
+++ UI/Registros/RegistroCategorias.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using StudioEA.BLL;
+using StudioEA.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -17,9 +19,111 @@ namespace StudioEA.UI.Registros
     /// </summary>
     public partial class RegistroCategorias : Window
     {
+        Categorias categorias = new Categorias();
+
         public RegistroCategorias()
         {
             InitializeComponent();
+            this.DataContext = categorias;
+            CategoriaIdTextBox.Text = "0";
+        }
+
+        private void Cargar()
+        {
+            this.DataContext = null;
+            this.DataContext = categorias;
+        }
+
+        private void Limpiar()
+        {
+            CategoriaIdTextBox.Text = "0";
+            UsuarioIdTextBox.Text = string.Empty;
+            NombreTextBox.Text = string.Empty;
+            categorias = new Categorias();
+            Cargar();
+        }
+
+        private void NuevoButton_Click(object sender, RoutedEventArgs e)
+        {
+            Limpiar();            
+        }
+
+        private bool ExisteEnLaBaseDeDatos()
+        {
+            Categorias c = CategoriasBLL.Buscar(categorias.CategoriaId);
+
+            return (c != null);
+        }
+
+
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            bool paso = false;
+
+            if (categorias.CategoriaId == 0)
+            {
+                paso = CategoriasBLL.Guardar(categorias);
+            }
+            else
+            {
+                if (ExisteEnLaBaseDeDatos())
+                {
+                    paso = CategoriasBLL.Modificar(categorias);
+                }
+                else
+                {
+                    MessageBox.Show("No Existe en la base de datos", "ERROR");
+                    return;
+                }
+
+                if (paso)
+                {
+                    Limpiar();
+                    MessageBox.Show("Guardado!!", "Exito");
+                }
+                else
+                {
+                    MessageBox.Show("No se pudo Guardar", "ERROR");
+                }
+            }
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (CategoriasBLL.Eliminar(categorias.CategoriaId))
+            {
+                Limpiar();
+                MessageBox.Show("Eliminado", "EXITO");
+            }
+            else
+            {
+                MessageBox.Show("No se pudo eliminar", "Error");
+            }
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            Categorias c = CategoriasBLL.Buscar(categorias.CategoriaId);
+
+            if (c != null)
+            {
+               categorias = c;
+                Cargar();
+            }
+            else
+            {
+                MessageBox.Show("No se encontro", "ERROR");
+            }
+        }
+
+        private void NombreTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            RegistroArticulos a = new RegistroArticulos();
+            string n = NombreTextBox.Text;
+
+            a.CategoriasTextBox.SelectedItem = n;
         }
     }
 }
