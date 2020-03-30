85905ea16ac83b6af8a0f023a289a643e8369dce
ErickVO <59853887+ErickVO@users.noreply.github.com>
2020-03-30 04:55:18 -0400

    Creacion de la Ventana Principal

---
+++ UI/Menu/VentanaPrincipal.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StudioEA.UI.Menu
+{
+    /// <summary>
+    /// Interaction logic for VentanaPrincipal.xaml
+    /// </summary>
+    public partial class VentanaPrincipal : Window
+    {
+        public VentanaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_4(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_5(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_6(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_7(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_8(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_9(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_10(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_11(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_12(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_13(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_14(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_15(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
