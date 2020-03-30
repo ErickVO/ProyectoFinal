f8db8586eddfcc9c07b10e3681adcb6c2c87cbef
Anthony Brian Jimenez Lopez <anthony_jimenez2@ucne.edu.do>
2020-03-30 04:02:04 -0400

    Agregando El Login

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using StudioEA.UI.Registros;
+using StudioEA.Entidades;
+using StudioEA.BLL;
 
 namespace StudioEA
 {
@@ -21,15 +23,39 @@ namespace StudioEA
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Usuarios> Lista = new List<Usuarios>();
+        public static int UsuarioId;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+
+        private void IngresarButton_Click_1(object sender, RoutedEventArgs e)
         {
             RUsuarios ru = new RUsuarios();
-            ru.Show();
+
+            Lista = UsuariosBLL.GetList(p => true);
+            bool paso = false;
+            foreach (var item in Lista)
+            {
+                if ((item.NombreUsuario == UsuarioTextBox.Text) && (item.Contrasena == ContrasenaBox.Password))
+                {
+                    UsuarioId = item.UsuarioId;
+                    ru.Show();
+                    paso = true;
+                    this.Close();
+                    break;
+                }
+            }
+            if (paso == false)
+            {
+                MessageBox.Show("Nombre de usuario o Contraseña incorrecto", "Error", MessageBoxButton.OK, MessageBoxImage.Information);
+                UsuarioTextBox.Text = string.Empty;
+                ContrasenaBox.Password = string.Empty;
+                UsuarioTextBox.Focus();
+            }
         }
     }
 }
