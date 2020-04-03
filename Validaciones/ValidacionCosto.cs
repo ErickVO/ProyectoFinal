f32f1e30d6e3cf4d64f8392e2ff2b0ba635744f2
ErickVO <59853887+ErickVO@users.noreply.github.com>
2020-04-03 01:12:37 -0400

    Validaciones

---
+++ Validaciones/ValidacionCosto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Controls;
+
+namespace StudioEA.Validaciones
+{
+    public class ValidacionCosto : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value != null)
+            {
+                decimal cantidad = 0;
+                try
+                {
+                    cantidad = Convert.ToDecimal(value);
+                }
+                catch
+                {
+                    return new ValidationResult(false, "El costo debe ser un numero");
+                }
+
+                if (cantidad >= 0.1m)
+                    return ValidationResult.ValidResult;
+                else
+                    return new ValidationResult(false, "El costo debe mayor o igual a 0.1");
+
+            }
+            return new ValidationResult(false, "Debes poner un costo");
+        }
+    }
+}    
+
+
---
+++ Validaciones/ValidacionPrecio.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Controls;
+
+namespace StudioEA.Validaciones
+{
+    public class ValidacionPrecio : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value != null)
+            {
+                decimal cantidad = 0;
+                try
+                {
+                    cantidad = Convert.ToDecimal(value);
+                }
+                catch
+                {
+                    return new ValidationResult(false, "El precio debe ser un numero");
+                }
+
+                if (cantidad >= 0.1m)
+                    return ValidationResult.ValidResult;
+                else
+                    return new ValidationResult(false, "El precio debe mayor o igual a 0.1");
+
+            }
+            return new ValidationResult(false, "Debes poner un precio");
+        }
+    }
+}
